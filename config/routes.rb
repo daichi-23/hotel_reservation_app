676fc93cc9cc9df47fc3659d3f56426b3e4130ef
Daichi Ito <daichi.sjtf@gmail.com>
2024-02-01 15:01:20 +0900

    デバイス、Userモデル完了まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
+  root to: 'home#index'
+  devise_for :users, controllers: { registrations: 'users/registrations' }#controllers以下あるとエラー
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'users/profile'
+  get 'users/profile_edit'
+  get 'users' => 'users#dummy'
+  resources:users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
